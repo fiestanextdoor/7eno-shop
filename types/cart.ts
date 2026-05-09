c03570f070d4312cf420e69a08c57c3c5945c6b6
joep <ps250954@summacollege.nl>
2026-05-09 14:08:37 +0200

    feat: add Printful and cart TypeScript types

---
+++ types/cart.ts
@@ -0,0 +1,10 @@
+export interface CartItem {
+  variantId: number
+  productId: number
+  productName: string
+  variantName: string
+  price: string
+  currency: string
+  quantity: number
+  imageUrl: string
+}
---
+++ types/printful.ts
@@ -0,0 +1,61 @@
+export interface SyncProduct {
+  id: number
+  name: string
+  thumbnail_url: string
+  variants: number
+  synced: number
+}
+
+export interface SyncVariant {
+  id: number
+  sync_product_id: number
+  name: string
+  retail_price: string
+  currency: string
+  is_ignored: boolean
+  sku: string
+  files: PrintfulFile[]
+  size: string
+  color: string
+  color_code: string
+  in_stock: boolean
+}
+
+export interface PrintfulFile {
+  type: string
+  id: number
+  url: string
+  options: unknown[]
+  hash: string
+  filename: string
+  mime_type: string
+  size: number
+  width: number
+  height: number
+  dpi: number
+  status: string
+  created: number
+  thumbnail_url: string
+  preview_url: string
+  visible: boolean
+}
+
+export interface PrintfulProductDetail {
+  sync_product: SyncProduct
+  sync_variants: SyncVariant[]
+}
+
+export interface PrintfulListResponse<T> {
+  code: number
+  result: T[]
+  paging: {
+    total: number
+    offset: number
+    limit: number
+  }
+}
+
+export interface PrintfulDetailResponse<T> {
+  code: number
+  result: T
+}
